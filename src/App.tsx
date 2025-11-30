cb4d08ec9a14521b98ffdc6e07b9cf6165989c80
krishi-patel25 <patelk4@unbc.ca>
2025-11-29 17:07:46 -0800

    Delete src directory

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-import LandingPage from './components/LandingPage';
-
-export default function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-
-
-  return <LandingPage onLogin={() => setIsAuthenticated(true)} />;
-}
